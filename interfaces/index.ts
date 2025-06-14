cf22bd354bd7eb00c209aa9b33d7097fb9fc9729
Juliet Wambuku <juliet@buupass.com>
2025-06-14 12:08:15 +0300

    First commit

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,31 +1,28 @@
-export interface CardProps {
-    title: string;
-    description: string;
-    imageUrl: string;
-  }
-  
-  export interface ButtonProps {
-    label: string;
-    onClick: () => void;
-  }
-  
+export interface Review {
+  name: string;
+  avatar: string;
+  rating: number;
+  comment: string;
+}
 
-  export interface PropertyProps {
+export interface Address {
+  city: string;
+  country: string;
+}
+
+export interface PropertyProps {
+  id: string;
+  name: string;
+  rating: number;
+  address: Address;
+  images: string[];
+  description: string;
+  amenities: string[];
+  price: number;
+  reviews: Review[];
+  host: {
     name: string;
-    address: {
-      state: string;
-      city: string;
-      country: string;
-    };
-    rating: number;
-    category: string[];
-    price: number;
-    offers: {
-      bed: string;
-      shower: string;
-      occupants: string;
-    };
-    image: string;
-    discount: string;
-  }
-  
+    avatar: string;
+    about: string;
+  };
+}
